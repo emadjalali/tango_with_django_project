6257076b6db8f4b72b20ec85b9a7eec2a9e31d27
Emad Jalali <Emad@emads-MacBook-Pro-3.local>
2017-02-14 20:37:56 +0000

    Chapter Seven

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
 
 def index(request):
 
